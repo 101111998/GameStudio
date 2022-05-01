f54293af7427a27ee7a01e63a1aab3a9a115928a
mo890qz <martin.onufrak@student.tuke.sk>
2022-05-01 19:06:07 +0200

    Update

--- src/main/java/connectFour/server/controller/ConnectFourController.java
+++ src/main/java/connectFour/server/controller/ConnectFourController.java
@@ -1,7 +1,9 @@
 package connectFour.server.controller;
 
 import connectFour.core.Field;
+import connectFour.core.GameState;
 import connectFour.core.Tile;
+import connectFour.entity.Score;
 import connectFour.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -10,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/connectfour")
@@ -21,9 +25,18 @@ public class ConnectFourController {
 
     @RequestMapping
     public String connectFour(@RequestParam(required = false) Integer column, Model model) {
-        if(column != null) field.placeToken(column);
-        model.addAttribute("scores", scoreService.getTopScores("connectfour"));
+        if(column != null){
+            field.placeToken(column);
+            if(field.getGameState() != GameState.PLAYING && field.getGameState() != GameState.DRAW){
+                scoreService.addScore(new Score("martin", "connectfour", field.getScore(), new Date()));
+            }
+        }
+        fillModel(model);
         return "connectfour";
+    }
+
+    private void fillModel(Model model){
+        model.addAttribute("scores", scoreService.getTopScores("connectfour"));
     }
 
     public String getHtmlField(){
@@ -46,7 +59,6 @@ public class ConnectFourController {
         return sb.toString();
     }
 
-
     private String getImageName(Tile tile) {
             switch (tile.getState()) {
                 case EMPTY:
@@ -65,8 +77,9 @@ public class ConnectFourController {
     }
 
     @RequestMapping("/new")
-    public String newGame(){
+    public String newGame(Model model){
         field = new Field(6, 7);
+        fillModel(model);
         return "connectfour";
     }
 }
--- src/main/java/connectFour/service/ServiceConfigJDBC.java
+++ src/main/java/connectFour/service/ServiceConfigJDBC.java
@@ -4,6 +4,6 @@ public class ServiceConfigJDBC {
 
     public static final String JDBC_URL = "jdbc:postgresql://localhost/gameStudio";
     public static final String JDBC_USER = "postgres";
-    public static final String JDBC_PASSWORD = "onufrakm10111998";
+    public static final String JDBC_PASSWORD = "admin1998";
 
 }
